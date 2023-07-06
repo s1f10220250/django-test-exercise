6f94ed8abb48fb7f344b7c6b4392ae86796db27b
1f10220250 <s1f102202505@iniad.org>
2023-07-06 13:55:36 +0900

    views.pyの改善part3

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from todo.models import Task
 
 # Create your views here.
 def index(request):
+    
     if request.method == 'POST':
         task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
@@ -19,4 +20,3 @@ def index(request):
     }
 
     return render(request, 'todo/index.html', context)
-
